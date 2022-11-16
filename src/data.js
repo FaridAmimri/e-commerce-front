069dd70617ae60d42035ca1e21378ff44dd1f2db
Farid Amimri <farid.amimri@gmail.com>
2022-11-16 17:17:26 +0100

    Create data file for saving sliders items

---
+++ src/data.js
@@ -0,0 +1,28 @@
+/** @format */
+
+export const sliderItems = [
+  {
+    id: 1,
+    image: 'assets/popularclothes.png',
+    title: 'POPULAR SALE',
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    background: 'fbf0f4'
+  },
+  {
+    id: 2,
+    image: 'assets/summerclothes.png',
+    title: 'SUMMER SALE',
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    background: 'f5fafd'
+  },
+  {
+    id: 3,
+    image: 'assets/autumnclothes.png',
+    title: 'AUTUMN COLLECTION',
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    background: 'fcf1ed'
+  }
+]
