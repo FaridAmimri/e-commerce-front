5aebf4150e18f0bafd96e94cf27c8212e8bb0c46
Farid Amimri <farid.amimri@gmail.com>
2022-11-16 17:16:09 +0100

    Add Slider component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 /** @format */
 
-import Announcement from './components/Announcement'
+import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
 
 function Home() {
   return (
     <div>
       <Announcement />
       <Navbar />
+      <Slider />
     </div>
   )
 }
