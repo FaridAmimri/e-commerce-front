2e50b15b92e1523e9951b7f05c541b7d0c4e4187
Farid Amimri <farid.amimri@gmail.com>
2022-11-17 11:45:02 +0100

    Add Categories component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@
 import Announcement from '../components/Announcement'
 import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
+import Categories from '../components/Categories'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   )
 }
