ddce79a978d8e43175c1c793cd28574536721a1a
Farid Amimri <farid.amimri@gmail.com>
2022-11-17 11:39:28 +0100

    Create caterory component rendering clothe type

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,48 @@
+/** @format */
+
+import styled from 'styled-components'
+import Button from '@mui/material/Button'
+
+function CategoryItem({ item }) {
+  return (
+    <Container>
+      <Image src={item.image} />
+      <Details>
+        <Title>{item.title}</Title>
+        <Button variant='contained'>SHOW NOW</Button>
+      </Details>
+    </Container>
+  )
+}
+
+export default CategoryItem
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+`
+
+const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`
+
+const Details = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+`
+
+const Title = styled.h1`
+  margin-bottom: 20px;
+`
