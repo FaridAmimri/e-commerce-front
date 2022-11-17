6b79390bb0ddeb01e255ec1e186215b5df48d3ff
Farid Amimri <farid.amimri@gmail.com>
2022-11-17 21:22:13 +0100

    Add Newletter component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
 import Products from '../components/Products'
+import Newsletter from '../components/Newsletter'
 
 function Home() {
   return (
@@ -14,6 +15,7 @@ function Home() {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter />
     </div>
   )
 }
