187cfb466323f1b09aedd30b1108b3414f237d24
Farid Amimri <farid.amimri@gmail.com>
2022-11-17 17:57:50 +0100

    Update image src file

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@
 export const sliderItems = [
   {
     id: 1,
-    image: 'assets/popularclothes.png',
+    image: 'assets/slides/slide1.png',
     title: 'POPULAR SALE',
     description:
       "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
@@ -11,7 +11,7 @@ export const sliderItems = [
   },
   {
     id: 2,
-    image: 'assets/summerclothes.png',
+    image: 'assets/slides/slide2.png',
     title: 'SUMMER SALE',
     description:
       "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
@@ -19,7 +19,7 @@ export const sliderItems = [
   },
   {
     id: 3,
-    image: 'assets/autumnclothes.png',
+    image: 'assets/slides/slide3.png',
     title: 'AUTUMN COLLECTION',
     description:
       "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
@@ -30,17 +30,52 @@ export const sliderItems = [
 export const categories = [
   {
     id: 1,
-    image: 'assets/style.png',
+    image: 'assets/categories/categorie1.png',
     title: 'SHIRT STYLE!'
   },
   {
     id: 2,
-    image: 'assets/loungewear.png',
+    image: 'assets/categories/categorie2.png',
     title: 'LOUNGEWEAR LOVE'
   },
   {
     id: 3,
-    image: 'assets/light.png',
+    image: 'assets/categories/categorie3.png',
     title: 'LIGHT JACKETS'
+  }
+]
+
+export const products = [
+  {
+    id: 1,
+    image: 'assets/products/product1.png'
+  },
+  {
+    id: 2,
+    image: 'assets/products/product2.png'
+  },
+  {
+    id: 3,
+    image: 'assets/products/product3.png'
+  },
+  {
+    id: 4,
+    image: 'assets/products/product4.png'
+  },
+  {
+    id: 5,
+    image: 'assets/products/product5.png'
+  },
+  {
+    id: 6,
+    image: 'assets/products/product6.png'
+  },
+  {
+    id: 7,
+    image: 'assets/products/product7.png'
+  },
+  {
+    id: 8,
+    image: 'assets/products/product8.png'
   }
 ]
