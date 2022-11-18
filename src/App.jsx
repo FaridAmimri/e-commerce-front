98704814d9271523d023f5bd1e0ad059b538bf88
Farid Amimri <farid.amimri@gmail.com>
2022-11-18 21:40:20 +0100

    Update images

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 /** @format */
 
 import HomePage from './pages/HomePage'
+import ProductPage from './pages/ProductPage'
 
 function App() {
   return (
     <div className='App'>
-      <HomePage />
+      <ProductPage />
     </div>
   )
 }
