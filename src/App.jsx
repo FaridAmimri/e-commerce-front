81409d27fb1e97a8f662d33410875cf8a725949b
Farid Amimri <farid.amimri@gmail.com>
2022-12-01 18:08:18 +0100

    Set Routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 /** @format */
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import Home from './pages/Home'
 import Products from './pages/Products'
+import Product from './pages/Product'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Cart from './pages/Cart'
 
 function App() {
+  const user = true
   return (
-    <div className='App'>
-      <Products />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products/:category' element={<Products />} />
+        <Route path='/product/:id' element={<Product />} />
+        <Route
+          path='/register'
+          element={user ? <Navigate to='/' /> : <Register />}
+        />
+        <Route path='/login' element={user ? <Navigate to='/' /> : <Login />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </Router>
   )
 }
 
