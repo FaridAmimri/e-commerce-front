07919e0c83670c462edee311e7a85b54638afc8f
Farid Amimri <farid.amimri@gmail.com>
2022-12-09 15:31:16 +0100

    Get token dynamically by fetching localStorage

--- src/requests.js
+++ src/requests.js
@@ -3,8 +3,9 @@
 import axios from 'axios'
 
 const BASE_URL = 'http://localhost:5000/api/'
-const TOKEN =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzODRjM2ZhM2Y0MTg2YWJlMjc2MjU5ZiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3MDMzNjgxMCwiZXhwIjoxNjcwNTk2MDEwfQ.bmFwXPz7cBptFL_4T9cLJ-fU7xb7PU_X8mmgJdo36Ao'
+
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user)
+  .currentUser.accessToken
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL
